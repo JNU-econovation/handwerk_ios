0d92af27795272684a057a7cb37eb71bc30b8f23
배성희 <shbae@baeseonghuiui-macbook-pro.local>
2019-01-01 20:04:35 +0900

    Initial Commit

---
+++ handwerk_second/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  handwerk_second
+//
+//  Created by 배성희 on 2019. 1. 1..
+//  Copyright © 2019년 배성희. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ handwerk_secondTests/handwerk_secondTests.swift
@@ -0,0 +1,34 @@
+//
+//  handwerk_secondTests.swift
+//  handwerk_secondTests
+//
+//  Created by 배성희 on 2019. 1. 1..
+//  Copyright © 2019년 배성희. All rights reserved.
+//
+
+import XCTest
+@testable import handwerk_second
+
+class handwerk_secondTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
